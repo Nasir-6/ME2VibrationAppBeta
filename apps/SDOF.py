f2e28a7d52cbbd5ad357b7ed8b7f87dbf1d719dd
Nasir <nasir-islam@hotmail.com>
2021-02-16 00:31:46 +0000

    SDOF Upgrade

--- apps/SDOF.py
+++ apps/SDOF.py
@@ -1,41 +1,126 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 import numpy as np
 import plotly.express as px
 
+import os
+
+
 from app import app
 
-layout = html.Div([
-    html.H3('Single Degree Of Freedom'),
+header = html.H3('Single Degree Of Freedom', className=" mt-1, text-center")
 
-    html.H6("Start Time"),
-    dcc.Input(id='start-time', type='number', min=0, max=100, step=0.1, value=0),
+line1_input = dbc.Row([
 
-    html.H6("End Time"),
-    dcc.Input(id='end-time', type='number', min=2, max=100, step=0.1, value=10),
+    dbc.Col(
+        html.Img(src=app.get_asset_url('SDOFPic.png'),
+                 className="img-fluid"),
+        className="col-12 col-sm-5 col-md-3 col-lg-3"),
+    dbc.Col([
+        dbc.Row(html.H6("System 1")),
+        dbc.Row([
+            dbc.Col(dbc.InputGroup(
+                [
+                    dbc.InputGroupAddon("Mass, m", addon_type="prepend"),
+                    dbc.Input(id="m", placeholder="kg", type="number", min=0, max=100, step=0.1),
+                ],
+            ), className="mb-1 col-12 col-sm-12 col-md-6"),
+            dbc.Col(dbc.InputGroup(
+                [
+                    dbc.InputGroupAddon("Spring Constant, k", addon_type="prepend"),
+                    dbc.Input(id="k", placeholder="N/m", type="number", min=0, max=100, step=0.1),
+                ],
+            ), className="mb-1 col-12 col-sm-12 col-md-6"),
+            dbc.Col(dbc.InputGroup(
+                [
+                    dbc.InputGroupAddon("Damping Coefficient, c", addon_type="prepend"),
+                    dbc.Input(id="c", placeholder="Ns/m", type="number", min=0, max=100, step=0.01),
+                ],
+            ), className="mb-1 col-12 col-sm-12 col-md-6"),
+            dbc.Col(dbc.InputGroup(
+                [
+                    dbc.InputGroupAddon("Initial Displacement, X0", addon_type="prepend"),
+                    dbc.Input(id="x0", placeholder="kg", type="number", min=0, max=100, step=0.1),
+                ],
+            ), className="mb-1 col-12 col-sm-12 col-md-6"),
+            dbc.Col(dbc.InputGroup(
+                [
+                    dbc.InputGroupAddon("Time Span, t", addon_type="prepend"),
+                    dbc.Input(id="tend", placeholder="s", type="number", min=0, max=10000, step=0.1),
+                ],
+            ), className="mb-1 col-12 col-sm-12 col-md-6"),
+            dbc.Col(dbc.InputGroup(
+                [
+                    dbc.InputGroupAddon("Number of Points", addon_type="prepend"),
+                    dbc.Input(id="n", placeholder="", type="number", min=10, max=10000, step=1, value=100),
+                ],
+            ), className="mb-1 col-12 col-sm-12 col-md-6"),
+            dbc.Button("Submit", color="secondary", id='submit-button-state', size="sm")
+        ])
+    ]),
+    # dbc.Col(,
+    #         width=1, className="align-self-end")
 
-    html.H6("Number of points"),
-    dcc.Input(id='npoints', type='number', min=2, max=10000, step=1, value=100),
+], className="jumbotron")
 
-    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+
+
+layout = dbc.Container([
+    header,
+    line1_input,
 
     dcc.Graph(id='sine_plot', figure={}),
 
-])
+], fluid=True)
 
 
 @app.callback(Output('sine_plot', 'figure'),
               Input('submit-button-state', 'n_clicks'),
-              State('start-time', 'value'),
-              State('end-time', 'value'),
-              State('npoints', 'value'))
-def update_output(n_clicks, tstart, tend, npoints):
-    print(tstart + tend)
-    tarr = np.linspace(tstart, tend, npoints)
-    y = np.sin(tarr)
+              State('m', 'value'),
+              State('k', 'value'),
+              State('c', 'value'),
+              State('x0', 'value'),
+              State('tend', 'value'),
+              State('n', 'value'))
+def update_output(n_clicks, m, k, c, x0, tend, n):
+    wn = np.sqrt(k / m)  # Natural Freq of spring mass system
+    dampRatio = c / (2 * np.sqrt(k * m))
+    tlim = 30
+    if tend < tlim:  # 30 is limit (Change this once I have a value)
+        t = np.linspace(0, tend, n)
+    else:
+        t = np.linspace(0, tlim, n)
+    x = t.copy()
 
-    fig = px.line(x=tarr, y=y)
+    if dampRatio == 0:
+        x = x0 * np.cos(wn * t)
+        solutionType = "Undamped Solution"
+    elif 1 > dampRatio > 0:
+        solutionType = "Under Damped Solution"
+        wd = wn * np.sqrt(1 - dampRatio ** 2)
+        A = x0
+        B = dampRatio * A / wd
+        x = np.exp(-dampRatio * wn * t) * (A * np.cos(wd * t) + B * np.sin(wd * t))
+    elif dampRatio == 1:
+        solutionType = "Critically Damped Solution"
+        A = x0
+        B = A * wn
+        x = (A + B * t) * np.exp(-wn * t)
+    elif dampRatio > 1:
+        solutionType = "Over Damped Solution"
+        A = x0 * (dampRatio + np.sqrt(dampRatio ** 2 - 1)) / (2 * np.sqrt(dampRatio ** 2 - 1))
+        B = x0 - A
+        x = A * np.exp((-dampRatio + np.sqrt(dampRatio ** 2 - 1)) * wn * t) + B * np.exp(
+            (-dampRatio - np.sqrt(dampRatio ** 2 - 1)) * wn * t)
+    else:
+        solutionType = "Unaccounted for Solution"
+
+
+# return x, t, solvable, solutionType
+    fig = px.line(x=t, y=x,
+                  labels=dict(x="Time (sec)", y="Displacement, x (m)"))
 
 
 
--- index.py
+++ index.py
@@ -1,28 +1,85 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 
 from app import app
 from app import server
 # Import all the diff app pages
 from apps import SDOF, ForcedVib, VibrationIsolation, BaseExcitation    #
 
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
-app.layout = html.Div([
-    html.H1("ME2 Vibrations App", className="title"),
-    html.Div([
-        dcc.Link('SDOF | ', href='/apps/SDOF'),
-        dcc.Link('Forced Vibrations', href='/apps/ForcedVib'),
-    ], className="pageLinks"),
+# Setting up Navigation Bar component
 
-    # dcc location is the way we change pages. so
+nav_bar_links = dbc.Row(
+    [
+        dbc.Nav(
+            [
+                dbc.NavItem(dbc.NavLink("SDOF", active="exact", href="/apps/SDOF")),
+                dbc.NavItem(dbc.NavLink("Forced Vibration", active="exact", href="/apps/ForcedVib")),
+                dbc.NavItem(dbc.NavLink("Vibration Isolation", active="exact", href="/apps/VibrationIsolation")),
+                dbc.NavItem(dbc.NavLink("Base Excitation", active="exact", href="/apps/BaseExcitation")),
+            ],
+            pills=True,
+        )
+    ],
+    no_gutters=True,
+    className="ml-auto flex-nowrap mt-3 mt-md-0",
+    align="center",
+)
+
+
+navbar = dbc.Navbar(
+    [
+        html.A(
+            dbc.Row(
+                [
+                    dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                    dbc.Col(dbc.NavbarBrand("ME2 Vibrations App", className="ml-2")),
+                ],
+                align="center",
+                no_gutters=True,
+            ),
+            href="https://plot.ly",   #Change LINK TO MAIN PAGE!!!
+        ),
+        dbc.NavbarToggler(id="navbar-toggler"),
+        # These are all the links to each app page, which collapses at small screens
+        dbc.Collapse(nav_bar_links, id="navbar-collapse", navbar=True),
+    ],
+    color="light",
+    dark=False,
+)
+
+
+
+# Structure of the App
+app.layout = dbc.Container([
+    navbar,
+    # dcc location is the way we change pages
     dcc.Location(id='url', pathname='', refresh=False),
     # All the content of each app goes in this DIV!!!!
     html.Div(id='page-content')
-])
+], fluid="True")
 
 
-# This is to change pages
+
+# Functions/Controller
+
+# Callback for toggling the collapse on small screens
+@app.callback(
+    Output("navbar-collapse", "is_open"),
+    Input("navbar-toggler", "n_clicks"),    # Input needed for callback!!!
+    State("navbar-collapse", "is_open"),
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+
+# Callback to change pages
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
 def display_page(pathname):
@@ -34,6 +91,7 @@ def display_page(pathname):
         return ForcedVib.layout
     else:
         return '404'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
